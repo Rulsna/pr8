3561be26bcc3d37d6ad1bfb3bee3546e7acef0af
Rulsna <mullabaevruslan@gmail.com>
2024-10-05 20:02:33 +0500

    Update 5.py

--- homework5/5.py
+++ homework5/5.py
@@ -1,12 +1,7 @@
-total = 0
-while True:
-    user_input = input("Введите число (или 'stop' для завершения): ")
-    
-    if user_input.lower() in ['stop', 'end']:
-        break
-    try:
-        number = float(user_input) 
-        total += number
-    except ValueError:
-        print("Пожалуйста, введите корректное число.")
-print(f"Сумма введенных чисел: {total}")
+summ = 0
+num = input("Введите число (или 'stop' для завершения): ")
+while num not in ['end','stop']:
+    summ += int(num)
+    num = input("Введите число (или 'stop' для завершения): ")
+else:
+    print(summ)
